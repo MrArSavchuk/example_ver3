9fd037371621df7ba95c7a659f95233d40c704e6
uliashem <shemiakinau@gmail.com>
2026-01-23 15:55:16 +0000

    added navbar-desktop

---
+++ src/features/Navbar/ui/NavbarDesktop/NavbarDesktop.jsx
@@ -0,0 +1,26 @@
+import { Button } from "@/shared/ui/Button";
+import { HashLink } from "react-router-hash-link"; 
+import { navigation } from "../../lib/data";
+import { useTranslation } from "react-i18next";
+import { Stack } from "@/shared/ui/Stack";
+import style from "./NavbarDesktop.module.scss";
+
+export const Navbar = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Stack tag="nav" aria-label="Main navigation" align="center">
+      <ul className={style.navigation}>
+        {Object.values(navigation).map(({ text, path }) => (
+          <li key={path}>
+            <HashLink smooth to={`#${path}`} 
+                className={style.navigationButton}
+              >
+              {t(text)}
+            </HashLink>
+          </li>
+        ))}
+      </ul>
+    </Stack>
+  );
+}
