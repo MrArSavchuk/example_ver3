0f96984b301679575794d374a75502b29881e5f5
natalia86901 <grishina.ermashkevich@gmail.com>
2026-01-26 17:08:12 -0800

    Добавила код, переместила папку с хуками в lib

---
+++ src/features/Navbar/lib/hooks/useOverflowHidden.js
@@ -0,0 +1,3 @@
+export { useOverflowHidden } from "./useOverflowHidden";
+export { useToggle } from "./useToggle";
+export { useWindowWidth } from "./useWindowWidth";
---
+++ src/features/Navbar/ui/NavbarMobile/NavbarMobile.jsx
@@ -0,0 +1,46 @@
+import { useOverflowHidden, useToggle, useWindowWidth } from "../../lib/hooks";
+import { HashLink } from "react-router-hash-link";
+import { navigation } from "../../lib/data";
+import { BurgerButton } from "./ui/BurgerButton";
+import { Stack } from "@/shared/ui/Stack";
+import { useTranslation } from "react-i18next";
+import style from "./NavbarMobile.module.scss";
+
+export const NavbarMobile = () => {
+  const { t } = useTranslation();
+  const { isOpen, toggle, close } = useToggle();
+  const width = useWindowWidth();
+  const isMobile = width <= 1024;
+  useOverflowHidden(isOpen);
+
+  return (
+    <Stack
+      tag="nav"
+      align="center"
+      className={style.navbarMobile}
+      aria-label="Main Navigation"
+      max
+    >
+      {isMobile && (
+        <BurgerButton
+          isOpen={isOpen}
+          toggleMenu={toggle}
+          className={style.burgerButton}
+          ariaLabel="Toggle Navbar"
+        />
+      )}
+
+      {isOpen && (
+        <ul className={style.menu}>
+          {Object.values(navigation).map(({ text, path }) => (
+            <li key={path} onClick={close}>
+              <HashLink smooth to={`#${path}`} className={style.sectionLink}>
+                {t(text).toUpperCase()}
+              </HashLink>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Stack>
+  );
+};
