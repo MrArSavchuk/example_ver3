f1f888dd1d886a8e0808749bc00ce273c357ce9a
Nati Zadepschi <zadepschi@yahoo.com>
2026-01-31 19:21:20 +0300

    Создана структура ServicesSection, добавлены компоненты, стили, бейдж и изображения

---
+++ src/widgets/ServicesSection/ui/ServiceItem/ServiceItem.jsx
@@ -0,0 +1,104 @@
+import React, { memo, useState } from "react";
+import { Typography } from "@/shared/ui/Typography";
+import { Stack } from "@/shared/ui/Stack";
+import desktopBadge from "@/shared/assets/images/most-popular-desktop.webp";
+import { ChevronDown, Minus, Plus } from "lucide-react";
+import styles from "./ServiceItem.module.scss";
+
+export const ServiceItem = memo(({ item, defaultOpen, children, value }) => {
+  if (!item) return null;
+
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+  const { _id, title, price, content } = item;
+
+  const titleDisplay = title || "Service name";
+  const priceDisplay = price || "—";
+  const contentDisplay = content || "Description is unavailable";
+
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const is3DVisionPack = String(_id) === String(value);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      handleIsOpen();
+    }
+  };
+
+  return (
+    <div className={styles.serviceItem}>
+      <Stack
+        className={styles.titleContainer}
+        justify="between"
+        align="center"
+        gap="16"
+        onClick={handleIsOpen}
+        aria-label="Service Item Button"
+        tabIndex="0"
+        role="button"
+        onKeyDown={handleKeyDown}
+      >
+        <Stack gap="24" align="center">
+          <ChevronDown
+            strokeWidth={1}
+            size={48}
+            className={isOpen ? styles.opened : styles.closed}
+          />
+          <Typography className={styles.title} font="poiretOne">
+            {titleDisplay}
+          </Typography>
+        </Stack>
+
+        {is3DVisionPack && (
+          <img
+            src={desktopBadge}
+            alt="Most Popular"
+            className={styles.badge}
+          />
+        )}
+
+        <Stack gap="32">
+          <Typography
+            className={styles.desktopPrice}
+            size="md"
+            font="poiretOne"
+          >
+            {priceDisplay}
+          </Typography>
+          {children}
+          {isOpen ? (
+            <Minus
+              className={styles.svgMobile}
+              strokeWidth={1}
+              size={32}
+            />
+          ) : (
+            <Plus
+              className={styles.svgMobile}
+              strokeWidth={1}
+              size={32}
+            />
+          )}
+        </Stack>
+      </Stack>
+
+      {isOpen && (
+        <Stack direction="column" gap="16" className={styles.list}>
+          <Typography className={styles.content}>
+            {contentDisplay}
+          </Typography>
+          
+          <Typography
+            className={styles.mobilePrice}
+            size="md"
+            font="poiretOne"
+          >
+            {priceDisplay}
+          </Typography>
+        </Stack>
+      )}
+    </div>
+  );
+});
