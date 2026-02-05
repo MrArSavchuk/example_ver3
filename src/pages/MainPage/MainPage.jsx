25dfc1130a023c062167225b1e2d340cdc4f1bc2
tamara-danilova2105 <sap1028@mail.ru>
2026-02-05 18:38:02 +0400

    refactor: App

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import { Stack } from "@/shared/ui/Stack"
 import { useTranslation } from "react-i18next"
-import { Contacts } from "../../widgets/contacts"
+import { Contacts } from "@/widgets/contacts"
 
 
 const MainPage = () => {
@@ -11,8 +11,6 @@ const MainPage = () => {
          align="center"
          gap="16"
        >
-         <h1>Olesya Martin</h1>
-         <p>{t('Interior Designer')}</p>
          <Contacts />
        </Stack>
      )
