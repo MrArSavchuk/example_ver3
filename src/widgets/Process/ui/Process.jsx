e566ba38295eb23ba4b4681ed886ed2d9c9d1258
tamara-danilova2105 <sap1028@mail.ru>
2026-02-07 18:21:55 +0400

    refactor

--- src/widgets/Process/ui/Process.jsx
+++ src/widgets/Process/ui/Process.jsx
@@ -12,54 +12,54 @@ export const Process = () => {
     const { t, i18n } = useTranslation();
 
     return (
-        <Stack tag="section" id="how-it-works" 
+        <Stack tag="section" id="how-it-works"
             direction="column" gap="32"
-            className={styles.sectionProcess} 
+            className={styles.sectionProcess}
         >
             <Stack direction="column" gap="16">
-                <TypographyV2 
-                    variant="h2" 
+                <TypographyV2
+                    variant="h2"
                     weight="bold"
                     className={styles.headingStyle}
                 >
-                    {t("HowItWorks")} 
+                    {t("HowItWorks")}
                 </TypographyV2>
-                <TypographyV2 
-                    variant="body16" 
+                <TypographyV2
+                    variant="body16"
                     className={`${styles.textStyle} ${styles.textWithHyphens}`}
-                    >
-                    {t("How It Works Text")} 
+                >
+                    {t("How It Works Text")}
                 </TypographyV2>
             </Stack>
 
-            <Stack 
+            <Stack
                 direction="column" gap="32" align="center"
                 className={styles.container}
             >
                 <picture className={styles.picture}>
-                    <img src={processImg} alt="Professional custom interior design Atlanta"  />
+                    <img src={processImg} alt="Professional custom interior design Atlanta" />
                 </picture>
 
                 <Stack direction="column" gap="32">
-                    {steps.map(({ index, title, text }, i) => 
+                    {steps.map(({ index, title, text }, i) =>
                         <Stack key={index} gap="32">
                             <Stack direction="column" gap="32">
                                 <Number index={index} />
-                                {i < steps.length - 1 && <Line/>}
+                                {i < steps.length - 1 && <Line />}
                             </Stack>
-                            <Stack direction="column" gap="16"> 
-                                <TypographyV2 
+                            <Stack direction="column" gap="16">
+                                <TypographyV2
                                     variant="h3"
                                     weight="bold"
                                 >
-                                    {t(title)} 
+                                    {t(title)}
                                 </TypographyV2>
                                 <TypographyV2
-                                    variant="body16" 
-                                    lang={i18n.language} 
+                                    variant="body16"
+                                    lang={i18n.language}
                                     className={`${styles.textProccess} ${styles.textWithHyphens}`}
                                 >
-                                    {t(text)} 
+                                    {t(text)}
                                 </TypographyV2>
                             </Stack>
                         </Stack>)}
