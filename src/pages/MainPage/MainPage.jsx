7e605b95bffd687fe247512abf91d0fba26bce64
Artem Savchuk <mrarsavchuk@gmail.com>
2026-02-14 06:38:50 -0500

    Revert "Switch Reviews sliders to Swiper API imports via local shim"
    This reverts commit 7829866593c052ff7284be9653dfef28891850b9.

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,29 +1,26 @@
-import { Contacts } from "@/widgets/contacts"
-import { ServicesSection } from "@/widgets/Services/ui"
-import { Header } from "@/widgets/Header"
-import { Footer } from "@/widgets/Footer"
-import { HeroSection } from "@/widgets/HeroSection"
-import { AboutOlesya } from "@/widgets/AboutOlesya"
-import { Process } from "@/widgets/Process"
-import { Stack } from "@/shared/ui/Stack"
-
+import { Contacts } from "@/widgets/Contacts";
+import { ServicesSection } from "@/widgets/Services/ui";
+import { Header } from "@/widgets/Header";
+import { Footer } from "@/widgets/Footer";
+import { HeroSection } from "@/widgets/HeroSection";
+import { AboutOlesya } from "@/widgets/AboutOlesya";
+import { Process } from "@/widgets/Process";
+import { ReviewsSection } from "@/widgets/Reviews";
+import { Stack } from "@/shared/ui/Stack";
 
 const MainPage = () => {
   return (
-    <Stack
-      direction="column"
-      align="center"
-      gap="64"
-    >
+    <Stack direction="column" align="center" gap="64">
       <Header />
       <HeroSection />
       <AboutOlesya />
       <ServicesSection />
       <Process />
+      <ReviewsSection />
       <Contacts />
       <Footer />
     </Stack>
-  )
-}
+  );
+};
 
 export default MainPage;
